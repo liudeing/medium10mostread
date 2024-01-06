bb1ce4dcc0d09017a88fe88c3ae5b6f634d7fc1c
ee <eexample@hotmail.com>
2024-01-06 17:17:55 +0800

    use puppeteer html to pdf

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CreatePDF msg="medium阅读量最高的10篇文章翻译"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CreatePDF from "@/components/CreatePDF.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CreatePDF
   }
 }
 </script>
